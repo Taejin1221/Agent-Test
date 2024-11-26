debbe9af00fd5ebcd6c09b95ed589b25d5391233
taejin1221 <taejin7824@gmail.com>
2024-11-26 13:21:35 +0900

    Feat: repository 추가
    - expense_repository, income_repository
    
    Related to: #1

---
+++ repository/income_repository.py
@@ -0,0 +1,47 @@
+import json
+from datetime import datetime
+
+from schemas.enum.income_category import IncomeCategory
+from schemas.income import Income
+
+
+class IncomeRepository:
+    def __init__(self, file_path: str):
+        self.file_path = file_path
+
+    def _read_data(self) -> dict:
+        with open(self.file_path, "r") as f:
+            return json.load(f)
+
+    def _write_data(self, data: dict) -> None:
+        with open(self.file_path, "w") as f:
+            json.dump(data, f, indent=4)
+
+    def add_income(
+            self,
+            date: datetime,
+            source: str,
+            amount: int,
+            category: IncomeCategory,
+            desc: str | None = None
+    ):
+
+        data = self._read_data()
+        expense = {
+            "date": date.strftime("%Y-%m-%d %H:%M:%S"),
+            "source": source,
+            "category": category.value,
+            "amount": amount,
+            "description": desc
+        }
+        data["incomes"].append(expense)
+        data["balances"] += amount
+        self._write_data(data)
+
+    def get_incomes(self) -> list[Income]:
+        data = self._read_data()
+        expenses = data["incomes"]
+
+        result = [Income(**expense) for expense in expenses]
+
+        return result
